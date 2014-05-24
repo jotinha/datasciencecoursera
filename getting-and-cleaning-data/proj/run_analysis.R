f76c8b67b2bc9ca9c94b19b46ea1c35db5756b75
jotinha <jotinha@github>
2014-05-24 20:39:00 +0100

    calculated tidydata2 and saved solutions to files

--- getting-and-cleaning-data/proj/run_analysis.R
+++ getting-and-cleaning-data/proj/run_analysis.R
@@ -10,7 +10,7 @@ loadTable <- function(folder) {
    dat <- read.table(paste(dir,'X_',folder,'.txt',sep=""),header=F,colClasses='numeric')
    
    #get feature names
-   featureNames <- read.table(paste(datadir,'features.txt',sep=""))
+   featureNames <- read.table(paste(DATADIR,'features.txt',sep=""))
    featureNames <- featureNames[featureNames[,1],2] #adjust by index of feature names given in first column of features.tx
    colnames(dat) <- featureNames
    
@@ -18,24 +18,47 @@ loadTable <- function(folder) {
    cols <- grepl('mean()',featureNames,fixed=T) | grepl('std()',featureNames,fixed=T)
    if (sum(cols) != 66) stop("Expecting 66 columns")
    dat <- subset(dat,select=cols)
+   
+   #rename colnames them to something more readable
+   colnames(dat) <- gsub("-mean\\(\\)-?","Mean",colnames(dat))
+   colnames(dat) <- gsub("-std\\(\\)-?","Std",colnames(dat))
       
    #load activities -----------------------------------------------------------------
    y <- read.table(paste(dir,'y_',folder,'.txt',sep=""))
    #give activities the proper labels
-   actLabels <- read.table(paste(datadir,'activity_labels.txt',sep=""))
-   dat$Activity <- factor(y[,1],levels=actLabels[,1],labels=actLabels[,2])
+   actLabels <- read.table(paste(DATADIR,'activity_labels.txt',sep=""))
+   dat$activity <- factor(y[,1],levels=actLabels[,1],labels=actLabels[,2])
    
    #load subject ---------------------------------------------------------------------
-   subj <- read.table(paste(dir,'subject_',folder,'.txt',sep=""),colClasses='factor')
-   dat$Subject <- subj[,1]
+   subj <- read.table(paste(dir,'subject_',folder,'.txt',sep=""),colClasses='integer')
+   dat$subject <- subj[,1]
    
    dat
 }
 
-train <- loadTable('train')
-test <- loadTable('test')
+loadAll <- function() {
+   train <- loadTable('train')
+   test <- loadTable('test')
+   
+   #make sure they have the same columns for fast merging
+   stopifnot(all(colnames(train) == colnames(test)))
+   
+   rbind(train,test)
+   
+}
 
-#make sure they have the same columns for fast merging
-stopifnot(all(colnames(train) == colnames(test)))
+createAverageTables <- function(dat) {
+   aggregate(. ~ activity + subject, data=dat, FUN=mean)
+}
 
-all <- rbind(train,test)
+saveData <- function(tidyDataAll,tidyDataAverages) {
+   #i chose csv for maximum compatibility
+   write.csv2(tidyDataAll,file="tidyDataAll.csv",row.names=F,)
+   write.csv2(tidyDataAverages,file="tidyDataAverages.csv",row.names=F)
+}
+
+dat <- loadAll()
+dat2 <- createAverageTables(dat)
+saveData(dat,dat2)
+
+
